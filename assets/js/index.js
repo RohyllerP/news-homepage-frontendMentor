3a917ab6f5bb7fc2f63465bc4b809bece7dcbddc
Rohyller Pereira <rohyler@gmail.com>
2023-07-24 01:50:35 -0400

    cambio-two

--- assets/js/index.js
+++ assets/js/index.js
@@ -4,16 +4,20 @@ const div = document.querySelector("#nav-oculto");
 const divContent = document.querySelector("#box-content");
 const body = document.querySelector("#body");
 const main = document.querySelector("#main");
+const headerTitle = document.querySelector("#header-title");
 
 btnOpen.onclick = function(){
     div.classList.remove("n-o");
     div.classList.add("n-c");
     divContent.classList.add("shadow-o");
     main.style.marginTop = "100px";
+    headerTitle.style.opacity = "0.3";
 }
 btnClose.onclick = function (){
     div.classList.remove("n-c");
     div.classList.add("n-o");
     divContent.classList.remove("shadow-o");
     main.style.marginTop = "0px";
+    headerTitle.classList.remove("shadow-o");
+    headerTitle.style.opacity = "1";
 }
